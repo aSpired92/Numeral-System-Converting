714409d76f4673e9a7def98590b01397f967478c
aSpired92 <maciek120920@gmail.com>
2020-03-04 22:44:47 +0100

    v1.1

--- src/main.cpp
+++ src/main.cpp
@@ -7,51 +7,53 @@
 
 using namespace std;
 
-string konwertuj(string konwertowana, int przed, int po);
+string convert(string toconv, int before, int after);
 string intToString(int integer);
 
 int main(int argc, char *argv[]) 
 {
 	system("@echo off && cls");
 
-	string liczba;
+	string number;
 	int presys, postsys;
 
-	cout << "Podaj liczbe: ";
-	cin >> liczba;
-	cout << "W ktorym systemie jest liczba?: ";
+	cout << "Number: ";
+	cin >> number;
+	cout << "Numeral system of number: ";
 	cin >> presys;
-	cout << "Na jaki system ja przekonwertowac?: ";
+	cout << "Convert to system: ";
 	cin >> postsys;
-	
-	cout<<endl<<"Liczba po konwersji: "<<konwertuj(liczba,presys,postsys)<<endl;
 
-	return 0;
+	cout<<endl<<"Number after conversion: "<<convert(number,presys,postsys)<<endl;
+
+	system("pause");
+	return EXIT_SUCCESS;
+	
 }
 
-string konwertuj(string konwertowana, int przed, int po)
+string convert(string toconv, int before, int after)
 {
-	int x = konwertowana.length(), suma = 0,a;
-	string cyfra;
+	int x = toconv.length(), sum = 0,a;
+	string digit;
 	for(int i = 0; i < x; i++)
 	{
-		cyfra = konwertowana[x - i - 1];
-		istringstream(cyfra)>>a;
-		suma+=a*pow(przed,i);
+		digit = toconv[x - i - 1];
+		istringstream(digit)>>a;
+		sum+=a*pow(before,i);
 	}
 
-	konwertowana = "";
+	toconv = "";
 
 	do
 	{
-		konwertowana += intToString(suma%po);
-		suma = suma/po;
+		toconv += intToString(sum%after);
+		sum = sum/after;
 	}
-	while(suma>0);
+	while(sum>0);
 
-	reverse(konwertowana.begin(), konwertowana.end());
+	reverse(toconv.begin(), toconv.end());
 
-	return konwertowana;
+	return toconv;
 
 }
 
